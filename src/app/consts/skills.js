67cffb9181dc325c984891f5fbce945d4425793d
BlackXDBD <musiicpromoter247@gmail.com>
2024-09-02 19:27:46 +0600

    Updates

--- src/app/consts/skills.js
+++
@@ -1,7 +0,0 @@
-export default {
-    language: ["js", "php", "html", "css", "c"],
-    database: ["sqlite", "postgreSql"],
-    wordPress: ["wordpressDev", "wordpressCo", "wordpressTD", "wordpressM", "wordpressS", "wprdpressSo"],
-    tools: ["elementor", "divi", "woocommerce", "jetPlugins", "sliderRevolution", "googleAnalytics", "contactForm", "crocoBlock"],
-    others: ["vscode", "figma", "git", "webpack", "canva"],      
-}
